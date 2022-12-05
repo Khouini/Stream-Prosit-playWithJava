ca8fda97d89dc5df721731a16ac88e7705aa9792
khouini <khouini.medyacine@gmail.com>
2022-12-05 11:56:35 +0100

    level 2

--- src/edu/esprit/game/levels/Level1.java
+++ src/edu/esprit/game/levels/Level1.java
@@ -6,60 +6,62 @@ import edu.esprit.game.utils.Data;
 import java.util.List;
 
 public class Level1 {
-	public static void main(String[] args) {
-		List<Employee> employees = Data.employees();
 
-		/* TO DO 1: Afficher tous les employ�s */
-                System.out.println("TO DO 1: Afficher tous les employ�s");
-		employees.stream().forEach((t)->System.out.println(t));
+    public static void main(String[] args) {
+        List<Employee> employees = Data.employees();
 
-		/*
+        /* TO DO 1: Afficher tous les employ�s */
+        System.out.println("TO DO 1: Afficher tous les employ�s");
+        employees.stream().forEach((t) -> System.out.println(t));
+
+        /*
 		 * TO DO 2: Afficher les employ�s dont le nom commence par la lettre n
-		 */
-                System.out.println("TO DO 2: Afficher les employ�s dont le nom commence par la lettre n");
-		employees.stream().filter((t)->t.getName().toLowerCase().startsWith("n")).forEach((t)->System.out.println(t));
+         */
+        System.out.println("TO DO 2: Afficher les employ�s dont le nom commence par la lettre n");
+        employees.stream().filter((t) -> t.getName().toLowerCase().startsWith("n")).forEach((t) -> System.out.println(t));
 
-		/*
+        /*
 		 * TO DO 3: Afficher les employ�s dont le nom commence par la lettre n
 		 * et le salaire> 1000
-		 */
-                System.out.println("TO DO 3: Afficher les employ�s dont le nom commence par la lettre n\n" +
-"		 * et le salaire> 1000");
-		employees.stream().filter((t)->t.getName().toLowerCase().startsWith("n")).filter((t)->t.getSalary()>1000)
-				.forEach((t)->System.out.println(t));
+         */
+        System.out.println("TO DO 3: Afficher les employ�s dont le nom commence par la lettre n\n"
+                + "		 * et le salaire> 1000");
+        employees.stream().filter((t) -> t.getName().toLowerCase().startsWith("n")).filter((t) -> t.getSalary() > 1000)
+                .forEach((t) -> System.out.println(t));
 
-		/*
+        /*
 		 * TO DO 4: Afficher les employ�s dont le nom commence par la lettre s
 		 * tri�s par salaire
-		 */
-                System.out.println("TO DO 4: Afficher les employ�s dont le nom commence par la lettre s\n" +
-"		 * tri�s par salaire");
-		employees.stream().filter((t)->t.getName().toLowerCase().startsWith("n")).sorted((t1, t2)->t1.getSalary()-t2.getSalary())
-				.forEach((t)->System.out.println(t));
+         */
+        System.out.println("TO DO 4: Afficher les employ�s dont le nom commence par la lettre s\n"
+                + "		 * tri�s par salaire");
+        employees.stream().filter((t) -> t.getName().toLowerCase().startsWith("n")).sorted((t1, t2) -> t1.getSalary() - t2.getSalary())
+                .forEach((t) -> System.out.println(t));
 
-                System.out.println("TO DO 5: Afficher les noms des employ�s dont le salaire > 600 avec 2\n" +
-"		 * mani�res diff�rentes");
-		/*
+        System.out.println("TO DO 5: Afficher les noms des employ�s dont le salaire > 600 avec 2\n"
+                + "		 * mani�res diff�rentes");
+        /*
 		 * TO DO 5: Afficher les noms des employ�s dont le salaire > 600 avec 2
 		 * mani�res diff�rentes
-		 */
-		/* First Way */
-                System.out.println("first way");
-		employees.stream().filter((e)->e.getSalary()>600).forEach((t)->System.out.println(t.getName()));
-		/* Second Way */
-                System.out.println("second way");
-		employees.stream().filter((e)->e.getSalary()>600).map((e)->e.getName())
-				.forEach((t)-> System.out.println(t));
+         */
+ /* First Way */
+        System.out.println("first way");
+        employees.stream().filter((e) -> e.getSalary() > 600).forEach((t) -> System.out.println(t.getName()));
+        /* Second Way */
+        System.out.println("second way");
+        employees.stream().filter((e) -> e.getSalary() > 600).map((e) -> e.getName())
+                .forEach((t) -> System.out.println(t));
 
-		/*
+        /*
 		 * TO DO 6: Ajouter 200 D pour les employ�s dont le nom commence avec m
 		 * et les affich�s ensuite
-		 */
-                System.out.println("tpdp6");
-		employees.stream().filter((e)->e.getName().toLowerCase().startsWith("m"))
-                        .forEach((e)-> {
-                            e.setSalary(e.getSalary()+200); 
-                            System.out.println(e);});
+         */
+        System.out.println("tpdp6");
+        employees.stream().filter((e) -> e.getName().toLowerCase().startsWith("m"))
+                .forEach((e) -> {
+                    e.setSalary(e.getSalary() + 200);
+                    System.out.println(e);
+                });
 
-	}
+    }
 }
--- src/edu/esprit/game/levels/Level2.java
+++ src/edu/esprit/game/levels/Level2.java
@@ -4,42 +4,42 @@ import edu.esprit.game.models.Employee;
 import edu.esprit.game.utils.Data;
 
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Level2 {
-	public static void main(String[] args) {
-	List<Employee> employees = Data.employees();
 
-	/* TO DO 1: Retourner le nombre des employ�s dont le nom commence avec n */
-	long nbr = employees.stream()./*TO DO 1 */
-				
-	/* TO DO 2: Retourner la somme des salaires de tous les employ�s (hint: mapToInt) */	
-	long sum = employees.stream()./* TO DO 2 */;	
-		
-	/* TO DO 3: Retourner la moyenne des salaires des employ�s dont le nom commence avec s */	
-	double average = employees.stream()./* TO DO 3 */;
-		
-		
-	/* TO DO 4: Retourner la liste de tous les employ�s  */	
-	List<Employee> emps = employees.stream()./* TO DO 4 */;
-		
-		/* TO DO 5: Retourner la liste des employ�s dont le nom commence par s */	
-		List<Employee> emps2 = employees.stream()./* TO DO 5 */;
-		
-		
-	/* TO DO 6: Retourner true si il y a au min un employ�s dont le salaire > 1000, false si non
-	*/
-	boolean test = employees.stream()./* TO DO 6 */
-				
-	/* TO DO 7: Afficher le premier employ� dont le nom commence avec s avec deux mani�res diff�rentes */
-	/*First way*/
-	employees.stream()./* TO DO 7 */
-	/*Second way*/
-	employees.stream()./* TO DO 7 */
-		
-	/* TO DO 8: Afficher le second employ� dont le nom commence avec s */
-	employees.stream()./* TO DO 8 */
-		
+    public static void main(String[] args) {
+        List<Employee> employees = Data.employees();
 
-	}
+        /* TO DO 1: Retourner le nombre des employ�s dont le nom commence avec n */
+        long nbr = employees.stream().filter(e -> e.getName().toLowerCase().startsWith("n")).count();
+        System.out.println("Le nombre des etudiants dont le nom commence par n est: " + nbr);
+        /* TO DO 2: Retourner la somme des salaires de tous les employ�s (hint: mapToInt) */
+        long sum = employees.stream().mapToInt(e -> e.getSalary()).sum()/* TO DO 2 */;
+        System.out.println("La somme des salaires de tout les employes est: " + sum + " dt");
+        /* TO DO 3: Retourner la moyenne des salaires des employ�s dont le nom commence avec s */
+        double average = employees.stream().mapToDouble(e -> e.getSalary()).average().getAsDouble();/* TO DO 3 */
+        System.out.println("la moyenne des salaires des employ�s dont le nom commence avec s est: " + average + " dt");
+        /* TO DO 4: Retourner la liste de tous les employ�s  */
+        List<Employee> emps = employees.stream().collect(Collectors.toList())/* TO DO 4 */;
+        System.out.println("Les liste est: " + emps);
+        /* TO DO 5: Retourner la liste des employ�s dont le nom commence par s */
+        List<Employee> emps2 = employees.stream().filter(e -> e.getName().toLowerCase().startsWith("s")).collect(Collectors.toList());/* TO DO 5 */;
+        System.out.println("la liste des employ�s dont le nom commence par s est: " + emps2);
+        /* TO DO 6: Retourner true si il y a au min un employ�s dont le salaire > 1000, false si non
+         */
+        boolean test = employees.stream().anyMatch(e -> e.getSalary() > 10000);/* TO DO 6 */
+        System.out.println("Retourner true si il y a au min un employ�s dont le salaire > 1000: " + test);
+        /* TO DO 7: Afficher le premier employ� dont le nom commence avec s avec deux mani�res diff�rentes */
+ /*First way*/
+         System.out.println("Afficher le premier employ� dont le nom commence avec s aves deux methodes");
+
+        employees.stream().filter(e->e.getName().toLowerCase().startsWith("s")).findFirst().ifPresent(e->System.out.println(e));/* TO DO 7 */
+        /*Second way*/
+        /* TO DO 8: Afficher le second employ� dont le nom commence avec s */
+        System.out.println("le second employ� dont le nom commence avec s : ");
+        employees.stream().filter(e->e.getName().toLowerCase().startsWith("s")).skip(1).findFirst().ifPresent(e->System.out.println(e));/* TO DO 7 */
+    }
 }
